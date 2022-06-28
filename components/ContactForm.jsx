08b29bc1f73b13eddf4566bf7612aa12a0d56c96
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-06-28 01:42:59 +0100

    form validation

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import Image from "next/image";
 import styled from "styled-components";
 import ViewLocation from "./ViewLocation";
 import { HeadSect } from "./WebDesign";
+import caution from "../public/assets/svg/caution.svg";
 
 const ContactSect = styled(HeadSect)`
   height: 764px;
@@ -14,6 +17,7 @@ const ContactSect = styled(HeadSect)`
     input,
     textarea {
       appearance: none;
+      outline: none;
       border: none;
       width: 100%;
       background: inherit;
@@ -22,6 +26,10 @@ const ContactSect = styled(HeadSect)`
       font-family: "JostMedium";
       font-size: 0.93em;
       color: var(--white);
+    }
+    input:focus,
+    textarea:focus {
+      border-bottom: 2px solid white;
     }
     input::placeholder,
     textarea::placeholder {
@@ -49,6 +57,26 @@ const ContactSect = styled(HeadSect)`
     }
   }
 
+  .inputDiv {
+    position: relative;
+
+    p {
+      position: absolute;
+      display: inline;
+      right: 0;
+      padding: 1.5em 0.5em;
+      margin: 0;
+      font-family: "JostRegular";
+      font-style: italic;
+      font-size: 0.75em;
+
+      span {
+        vertical-align: sub;
+        padding-left: 0.5em;
+      }
+    }
+  }
+
   @media screen and (min-width: 641px) {
     article {
       max-width: 80%;
@@ -63,6 +91,7 @@ const ContactSect = styled(HeadSect)`
     .form {
       display: flex;
       flex-direction: column;
+      width: 100%;
     }
     button {
       align-self: flex-end;
@@ -85,6 +114,65 @@ const ContactSect = styled(HeadSect)`
 `;
 
 function ContactForm() {
+  const [errors, setErrors] = useState("");
+  const [mailError, setMailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [msgError, setMsgError] = useState("");
+
+  const validateName = (e) => {
+    if (e.target.value?.length < 2) {
+      setErrors(`Can't be empty`);
+      return false;
+    } else {
+      setErrors("");
+      console.log(e.target.value);
+      return true;
+    }
+  };
+
+  const validateText = (e) => {
+    if (e.target.value?.length < 4) {
+      setMsgError("Can't be empty");
+      return false;
+    } else {
+      setMsgError("");
+      console.log(e.target.value);
+      return true;
+    }
+  };
+
+  const validateMail = (e) => {
+    if (e.target.validity.typeMismatch) {
+      setMailError("Invalid format");
+      return false;
+    } else if (e.target.value?.length === 0) {
+      setMailError("Can't be empty");
+      return false;
+    } else {
+      setMailError("");
+      return true;
+    }
+  };
+
+  const validatePhone = (e) => {
+    if (e.target.validity.typeMismatch) {
+      setPhoneError("Invalid format");
+      return false;
+    } else if (e.target.value?.length === 0) {
+      setPhoneError("Can't be empty");
+      return false;
+    } else {
+      setPhoneError("");
+      return true;
+    }
+  };
+
+  const submit = (e) => {
+    if (!validateMail() || !validateText() || !validateName()) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <>
       <ContactSect>
@@ -101,22 +189,60 @@ function ContactForm() {
 
           <div className="form">
             <form action="">
-              <input type="text" name="name" id="name" placeholder="Name" />
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email Address"
-              />
-              <input type="tel" name="phone" id="phone" placeholder="Phone" />
-              <textarea
-                name="placeholder"
-                id="placeholder"
-                rows="4"
-                placeholder="Your Message"
-              ></textarea>
+              <div className="inputDiv">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Name"
+                  onBlur={validateName}
+                />
+                <p>
+                  {errors}
+                  <span>{errors && <Image src={caution} alt="" />}</span>
+                </p>
+              </div>
+              <div className="inputDiv">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email Address"
+                  onBlur={validateMail}
+                />
+                <p>
+                  {mailError}
+                  <span>{mailError && <Image src={caution} alt="" />}</span>
+                </p>
+              </div>
+              <div className="inputDiv">
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  placeholder="Phone"
+                  onBlur={validatePhone}
+                />
+                <p>
+                  {phoneError}
+                  <span>{phoneError && <Image src={caution} alt="" />}</span>
+                </p>
+              </div>
+              <div className="inputDiv">
+                <textarea
+                  name="placeholder"
+                  id="placeholder"
+                  rows="4"
+                  placeholder="Your Message"
+                  onBlur={validateText}
+                ></textarea>
+                <p>
+                  {msgError}
+                  <span>{msgError && <Image src={caution} alt="" />}</span>
+                </p>
+              </div>
             </form>
-            <button>Submit</button>
+            <button onSubmit={submit}>Submit</button>
           </div>
         </article>
       </ContactSect>
